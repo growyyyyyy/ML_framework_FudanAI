79193506f86a9481bd2a5a492980e9fe6e28b26f
杨乐宜 <773323756@qq.com>
2025-05-25 15:48:35 +0800

    清空

--- fudanai/data/dataset.py
+++
@@ -1,9 +0,0 @@
-class Dataset:
-    def __init__(self, xs, ys):
-        self.xs, self.ys = xs, ys
-
-    def __len__(self):
-        return len(self.xs)
-
-    def __getitem__(self, idx):
-        return self.xs[idx], self.ys[idx]
--- fudanai/layers/activation.py
+++
@@ -1,7 +0,0 @@
-from fudanai.core.tensor import Tensor
-from fudanai.core.module import Module
-
-
-class ReLU(Module):
-    def forward(self, x: Tensor):
-        return x.relu()
