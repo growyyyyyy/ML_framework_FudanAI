549de0a0d3fed5db8dec192854c5ca1230b8776c
杨乐宜 <773323756@qq.com>
2025-05-28 16:28:17 +0800

    update example.ipynb

--- src/graph.py
+++ src/graph.py
@@ -1,20 +1,19 @@
-import networkx as nx
-from networkx.drawing.nx_agraph import to_agraph
+# src/graph.py
 
+import pydot
 
 class CausalDAG:
     def __init__(self, dot_str: str):
         self.dot = dot_str
-        self.g = nx.DiGraph(
-            nx.drawing.nx_agraph.from_agraph(to_agraph(dot_str)))
 
     @classmethod
     def from_dot(cls, dot_str: str):
+        # 只保存字符串，不转换成 networkx
         return cls(dot_str)
 
-    def to_png(self, out_path="causal_model.png", size=(16, 14)):
-        A = to_agraph(self.dot)
-        A.graph_attr.update(size="{},{}".format(*size))
-        A.layout('dot')
-        A.draw(out_path)
-        return out_path
+    def to_png(self, path: str):
+        # 直接让 pydot 把 dot 渲染为 PNG
+        graphs = pydot.graph_from_dot_data(self.dot)
+        if not graphs:
+            raise ValueError("无法解析 DOT 数据")
+        graphs[0].write_png(path)
